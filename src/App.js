b69e6cc305cb1fbdb2e6c8a371c8d6248ba7f784
Branda0 <g.brandao.dev@gmail.com>
2022-05-05 14:09:07 +0200

    working on base page

--- src/App.js
+++ src/App.js
@@ -1,44 +1,62 @@
 import "./App.scss";
 import logo from "./assets/img/logo.png";
+import logoGithub from "./assets/img/github.svg";
 
 function App() {
   return (
     <div className="page">
-      <div className="header">
-        <div className="container header__wrapper">
-          <div className="logo">
-            <img src={logo} alt="" />
-          </div>
-          <p>Salut</p>
-          <nav></nav>
-        </div>
-      </div>
+      <section className="header">
+        <div className="header__wrapper">
+          {/* <div className="header_logo"> */}
+          <img className="header__logo" src={logo} alt="" />
 
-      <div className="hero">
+          <nav className="nav">
+            <p>Salut</p>
+            <img className="nav__github" src={logoGithub} alt="" />
+            <button className="button nav__button">
+              <span className="text"> Mon CV</span>
+            </button>
+          </nav>
+        </div>
+      </section>
+
+      <section className="hero">
         <div className="container">
-          {/* <p className="hero__hello">Bonjour, je m'appelle</p> */}
           <h2 className="hero__name">Gabriel Brandao</h2>
           <p className="hero__dev">Développeur FullStack</p>
-          {/* </div> */}
+          {/* <p className="hero__text">
+            Diplomé d'<strong>Ingénieur</strong> en <strong> calcul scientifique </strong> /
+            <strong> mécanique numérique</strong> et désirant m'orienter vers une carrière dans le
+            <strong> développement web</strong>, j'ai suivis une formation intensive auprès du centre
+            <strong> Le Reacteur</strong>.
+          </p> */}
         </div>
-      </div>
+      </section>
 
-      {/* <div className="container"> */}
-
-      <div className="about">
-        <div className="container">
-          <p className="about__title">
-            Diplomé d'Ingénieur en calcul scientifique / mécanique numérique et désirant m'orienter vers les
-            métiers du web j'ai suivis une formation intensive auprès du centre Le Reacteur.
+      <section className="about">
+        <div className="container about__wrapper">
+          <p className="about__text">
+            Diplomé d'<strong>Ingénieur</strong> en <strong> calcul scientifique </strong> /
+            <strong> mécanique numérique</strong> et désirant m'orienter vers une carrière dans le
+            <strong> développement web</strong>, j'ai suivis une formation intensive auprès du centre
+            <strong> Le Reacteur</strong>. J'utilise <strong>Javascript</strong>, <strong>HTML</strong>,
+            <strong> CSS </strong>et plus particulièrement <strong>React</strong> et <strong>NodeJS</strong>{" "}
+            avec la stack <strong>MERN</strong>.
           </p>
+          <div className="about__mern">
+            <img className="mern__mongodb" src="" alt="" />
+            <img className="mern__express" src="" alt="" />
+            <img className="mern__react" src="" alt="" />
+            <img className="mern__nodejs" src="" alt="" />
+          </div>
         </div>
-      </div>
+      </section>
 
-      <div className="projects">
+      <section className="projects">
         <div className="container">
           <h4 className="projects__title"></h4>
         </div>
-      </div>
+      </section>
     </div>
   );
 }
---
+++ src/components/Project.js
@@ -0,0 +1,5 @@
+const Project = () => {
+  return <span>CARD</span>;
+};
+
+export default Project;
